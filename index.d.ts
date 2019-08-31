5c61b7c466120ba69cc310f522920d86c885bed0
Alex Dixon <alex@conarrative.com>
2019-08-30 19:42:01 -0700

    Update types

--- index.d.ts
+++ index.d.ts
@@ -1,35 +1,53 @@
+
+
 declare module "framework-x" {
-  export type Db = object
-  export type EventType = string
-  export type Payload = any
-  export type Event = [EventType, Payload?]
+  export type EventName = string
+  export type EventData = any
+  export type Event = [EventName, EventData?]
   export type DbFn = (db: Db) => Db
-  export type FxName = "db" | any
+  export type FxName = "db" | "dispatch" | string
   export type FxArg = any
   export type Effects = { db: DbFn | Db } & { [k in FxName]: FxArg }
+    | [FxName, FxArg][]
+
+  export type Db = object
+
+  export function dispatch(...event: Event): void;
+  export function dispatch(eventType: EventName, ...args: [any]): void;
+
+  export type Coeffects = { db: Db } & { [k: string]: any }
+
+  export type Handler = (cofx: Coeffects, eventType: EventName, args: EventData) => Effects | void
+
+  export function regEventFx(eventType: EventName, handler: Handler): void;
 
   export interface Effect {
     (x: any): any
   }
 
-  export function dispatch(...event: Event): void;
-  export function dispatch(eventType: EventType, ...args: [any]): void;
-
-  export type Coeffects = { db: Db } & { [k: string]: any }
-
-  export type Handler = (cofx: Coeffects, eventType: EventType, args: Payload) => Effects | void
-
-  export function regEventFx(eventType: EventType, handler: Handler): void;
-
-  export function regFx(name: FxName, fn: Effect):void
+  export function regFx(name: FxName, fn: Effect): void
 
   export function getState(): Db
 
-  type StateListener = (db: Db) => any
+  type StateListener = (db: Db) => void
 
   export function subscribeToState(fn: StateListener): void
 
-  type Middleware = (type: string, payload: any, effects: any) => any
+  type EventListener = (type: string, payload: any, effects: any) => any
 
-  export function createStore(initialState?: object): { dispatch, getState, setState, regEventFx, regFx, subscribeToState }
+  export interface Store {
+    getState(): Db
+
+    dispatch(...event: Event): void;
+
+    dispatch(eventType: EventName, ...args: [any]): void;
+
+    regEventFx(eventType: EventName, handler: Handler): void;
+
+    regFx(name: FxName, fn: Effect): void
+
+    subscribeToState(fn: StateListener): void
+  }
+
+  export function createStore(initialState?: object): Store
 }
