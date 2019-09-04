70fcc806d54e6e072353e4811df02139ae522560
mikegai <mike.d.gai@gmail.com>
2019-09-04 11:18:28 -0700

    group tests under describe block

--- src/core.test.js
+++ src/core.test.js
@@ -12,90 +12,92 @@ export const updateIn = R.curry((ks, f, m) =>
 const dbFx = reducerOrState => ['db', reducerOrState]
 const dispatchFx = (type, payload) => ['dispatch', [type, payload]]
 
-it('should reduce a simple event', () => {
-  const { dispatch, regEventFx, getState } = createStore()
-  regEventFx(evt.MESSAGE, (_, message) =>
-    [dbFx(R.assoc('message', message))])
-  dispatch(evt.MESSAGE, 'hello')
-  expect(getState()).toEqual({ message: 'hello' })
-})
-
-it('should process dispatch child event synchronously', () => {
-  const { dispatch, regEventFx, getState } = createStore()
-  regEventFx(evt.MESSAGE, (_, message) => [
-    dbFx(updateIn(['messages'], R.append(message))),
-    dispatchFx(evt.SUBEVENT, message),
-    dbFx(updateIn(['messages'], R.append('end')))
-  ])
-  regEventFx(evt.SUBEVENT, (_, message) => [
-    dbFx(updateIn(['messages'], R.append(`sub ${message}`)))
-  ])
-  dispatch(evt.MESSAGE, 'hello')
-  expect(getState()).toEqual({ 'messages': ['hello', 'sub hello', 'end'] })
-})
-
-it('should permit custom fx', () => {
-  const { regFx, regEventFx, getState, dispatch } = createStore()
-  let afterCntr = 2
-  regFx('custom', (_, amount) => {
-    afterCntr = afterCntr + amount
+describe('core dispatch flow', () => {
+  it('should reduce a simple event', () => {
+    const { dispatch, regEventFx, getState } = createStore()
+    regEventFx(evt.MESSAGE, (_, message) =>
+      [dbFx(R.assoc('message', message))])
+    dispatch(evt.MESSAGE, 'hello')
+    expect(getState()).toEqual({ message: 'hello' })
   })
-  regEventFx(evt.MESSAGE, (_, message) => [
-    dbFx(updateIn(['messages'], R.append(message))),
-    dispatchFx(evt.SUBEVENT, message),
-    dbFx(updateIn(['messages'], R.append('end')))
-  ])
-  regEventFx(evt.SUBEVENT, (_, message) => [
-    dbFx(updateIn(['messages'], R.append(`sub ${message}`))),
-    ['custom', 5]
-  ])
-  dispatch(evt.MESSAGE, 'hello')
-  expect(getState()).toEqual({ 'messages': ['hello', 'sub hello', 'end'] })
-  expect(afterCntr).toEqual(7)
-})
 
-it('should notify subscribers of state change', () => {
-  const { regEventFx, regFx, dispatch, subscribeToState } = createStore()
-  let stateCntr = 0
-  let state = null
-  subscribeToState(newState => {
-    stateCntr++
-    state = newState
+  it('should process dispatch child event synchronously', () => {
+    const { dispatch, regEventFx, getState } = createStore()
+    regEventFx(evt.MESSAGE, (_, message) => [
+      dbFx(updateIn(['messages'], R.append(message))),
+      dispatchFx(evt.SUBEVENT, message),
+      dbFx(updateIn(['messages'], R.append('end')))
+    ])
+    regEventFx(evt.SUBEVENT, (_, message) => [
+      dbFx(updateIn(['messages'], R.append(`sub ${message}`)))
+    ])
+    dispatch(evt.MESSAGE, 'hello')
+    expect(getState()).toEqual({ 'messages': ['hello', 'sub hello', 'end'] })
   })
-  regEventFx(evt.MESSAGE, (_, message) => [
-    dbFx(R.assoc('message', message)),
-  ])
-  dispatch(evt.MESSAGE, 'hello')
-  expect(stateCntr).toEqual(1)
-})
 
-it('should notify subscribers only once of state change', () => {
-  const { regEventFx, regFx, dispatch, subscribeToState } = createStore()
-  let stateCntr = 0
-  let state = null
-  let customCntr = 0
-  subscribeToState(newState => {
-    stateCntr++
-    state = newState
+  it('should permit custom fx', () => {
+    const { regFx, regEventFx, getState, dispatch } = createStore()
+    let afterCntr = 2
+    regFx('custom', (_, amount) => {
+      afterCntr = afterCntr + amount
+    })
+    regEventFx(evt.MESSAGE, (_, message) => [
+      dbFx(updateIn(['messages'], R.append(message))),
+      dispatchFx(evt.SUBEVENT, message),
+      dbFx(updateIn(['messages'], R.append('end')))
+    ])
+    regEventFx(evt.SUBEVENT, (_, message) => [
+      dbFx(updateIn(['messages'], R.append(`sub ${message}`))),
+      ['custom', 5]
+    ])
+    dispatch(evt.MESSAGE, 'hello')
+    expect(getState()).toEqual({ 'messages': ['hello', 'sub hello', 'end'] })
+    expect(afterCntr).toEqual(7)
   })
-  regFx('custom', () => {
-    customCntr++
+
+  it('should notify subscribers of state change', () => {
+    const { regEventFx, regFx, dispatch, subscribeToState } = createStore()
+    let stateCntr = 0
+    let state = null
+    subscribeToState(newState => {
+      stateCntr++
+      state = newState
+    })
+    regEventFx(evt.MESSAGE, (_, message) => [
+      dbFx(R.assoc('message', message))
+    ])
+    dispatch(evt.MESSAGE, 'hello')
+    expect(stateCntr).toEqual(1)
   })
-  regEventFx(evt.MESSAGE, (_, message) => [
-    dbFx(R.assoc('message', message)),
-    dispatchFx(evt.SUBEVENT, message),
-    dbFx(R.assoc('done', true))
-  ])
-  regEventFx(evt.SUBEVENT, (_, message) => [
-    ['custom', 'yep'],
-    dbFx(updateIn(['messages'], R.append(`sub ${message}`)))
-  ])
-  dispatch(evt.MESSAGE, 'hello')
-  // only 1, not 3
-  expect(stateCntr).toEqual(1)
-  expect(customCntr).toEqual(1)
-  expect(state).toEqual({
-    'done': true, 'message': 'hello', 'messages': ['sub hello']
+
+  it('should notify subscribers only once of state change', () => {
+    const { regEventFx, regFx, dispatch, subscribeToState } = createStore()
+    let stateCntr = 0
+    let state = null
+    let customCntr = 0
+    subscribeToState(newState => {
+      stateCntr++
+      state = newState
+    })
+    regFx('custom', () => {
+      customCntr++
+    })
+    regEventFx(evt.MESSAGE, (_, message) => [
+      dbFx(R.assoc('message', message)),
+      dispatchFx(evt.SUBEVENT, message),
+      dbFx(R.assoc('done', true))
+    ])
+    regEventFx(evt.SUBEVENT, (_, message) => [
+      ['custom', 'yep'],
+      dbFx(updateIn(['messages'], R.append(`sub ${message}`)))
+    ])
+    dispatch(evt.MESSAGE, 'hello')
+    // only 1, not 3
+    expect(stateCntr).toEqual(1)
+    expect(customCntr).toEqual(1)
+    expect(state).toEqual({
+      'done': true, 'message': 'hello', 'messages': ['sub hello']
+    })
   })
 })
 
@@ -135,6 +137,5 @@ describe('creation with specified env', () => {
     expect(Object.isFrozen(env)).toEqual(true)
     expect(env.eventFx['my-evt'].length).toEqual(2)
     expect(env.eventFx['my-evt'][1].name).toEqual('dynRegister')
-
   })
 })
