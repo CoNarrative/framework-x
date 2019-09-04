5bbfaaf53b8642f792544fb47bdce3e4bd0cacdf
mikegai <mike.d.gai@gmail.com>
2019-09-03 18:51:18 -0700

    make dispatch synchronous

---
+++ src/core.test.js
@@ -0,0 +1,183 @@
+import * as R from 'ramda'
+import { createStore } from './createStore'
+
+const evt = {
+  MESSAGE: 'message',
+  SUBEVENT: 'subevent'
+}
+
+export const updateIn = R.curry((ks, f, m) =>
+  R.assocPath(ks, f(R.path(ks, m)), m)
+)
+const dbFx = reducerOrState => ['db', reducerOrState]
+const dispatchFx = (type, payload) => ['dispatch', [type, payload]]
+
+it('should reduce a simple event', () => {
+  const { dispatch, regEventFx, getState } = createStore()
+  regEventFx(evt.MESSAGE, (_, message) =>
+    [dbFx(R.assoc('message', message))])
+  dispatch(evt.MESSAGE, 'hello')
+  expect(getState()).toEqual({ message: 'hello' })
+})
+
+it('should process dispatch child event synchronously', () => {
+  const { dispatch, regEventFx, getState } = createStore()
+  regEventFx(evt.MESSAGE, (_, message) => [
+    dbFx(updateIn(['messages'], R.append(message))),
+    dispatchFx(evt.SUBEVENT, message),
+    dbFx(updateIn(['messages'], R.append('end')))
+  ])
+  regEventFx(evt.SUBEVENT, (_, message) => [
+    dbFx(updateIn(['messages'], R.append(`sub ${message}`)))
+  ])
+  dispatch(evt.MESSAGE, 'hello')
+  expect(getState()).toEqual({'messages': ['hello', 'sub hello', 'end'] })
+})
+
+xit('should permit custom fx and those can chain', () => {
+  const { regFx, regEventFx, reduceDispatch } = createStore()
+  let afterCntr = 0
+  regFx('custom', () => {
+    afterCntr++
+  })
+  regEventFx(evt.MESSAGE, (_, message) => [
+    dbFx(updateIn(['messages'], R.append(message))),
+    dispatchFx(evt.SUBEVENT, message),
+    dbFx(updateIn(['messages'], R.append('end')))
+  ])
+  regEventFx(evt.SUBEVENT, (_, message) => [
+    dbFx(updateIn(['messages'], R.append(`sub ${message}`))),
+    ['custom', 'yep']
+  ])
+  const result = reduceDispatch(evt.MESSAGE, 'hello')
+  expect(R.omit(['afterFx'], result)).toEqual({
+    'db': { 'messages': ['hello', 'sub hello', 'end'] },
+    'lastEventType': 'subevent',
+    'stateIsDirty': true,
+    supplied: [],
+    requires: []
+  })
+  expect(result.afterFx.length).toBe(1)
+  expect(afterCntr).toEqual(0)
+})
+
+xit('should notify subscribers only once and apply afterFx', () => {
+  const { regEventFx, regFx, dispatch, subscribeToState } = createStore()
+  let stateCntr = 0
+  let state = null
+  let afterCntr = 0
+  subscribeToState(newState => {
+    stateCntr++
+    state = newState
+  })
+  regFx('custom', () => {
+    afterCntr++
+  })
+  regEventFx(evt.MESSAGE, (_, message) => [
+    dbFx(R.assoc('message', message)),
+    dispatchFx(evt.SUBEVENT, message),
+    dbFx(R.assoc('done', true))
+  ])
+  regEventFx(evt.SUBEVENT, (_, message) => [
+    ['custom', 'yep'],
+    dbFx(updateIn(['messages'], R.append(`sub ${message}`)))
+  ])
+  dispatch(evt.MESSAGE, 'hello')
+  expect(stateCntr).toEqual(1)
+  expect(afterCntr).toEqual(1)
+  expect(state).toEqual({
+    'done': true, 'message': 'hello', 'messages': ['sub hello']
+  })
+})
+
+const id = ['id']
+
+xit('should block and ask for coeffects', () => {
+  const { regEventFx, reduceDispatch } = createStore()
+  regEventFx(evt.MESSAGE, { id }, () => {
+    throw new Error('Should never get to me (at least how we are handling coeffects now)')
+  })
+  const next = reduceDispatch(evt.MESSAGE, 'hello')
+
+  expect(next).toEqual({
+    afterFx: [], db: {}, requires: [{ id: ['id'] }], supplied: []
+  })
+})
+
+xit('should work all the way through if supplied coeffects', () => {
+  const { regEventFx, reduceDispatchStateless } = createStore()
+  regEventFx(evt.MESSAGE, { id }, ({ id }) => [
+    dbFx(R.assoc('id', id)),
+    dbFx(R.assoc('done', true))
+  ])
+  const next = reduceDispatchStateless({
+    db: {},
+    requires: [],
+    supplied: [{ id: '88' }],
+    afterFx: []
+  }, [evt.MESSAGE, 'hello'])
+
+  expect(R.omit(['lastEventType'], next)).toEqual({
+    afterFx: [],
+    db: {
+      done: true,
+      id: '88'
+    },
+    stateIsDirty: true,
+    requires: [{ id: ['id'] }],
+    supplied: [{ id: '88' }],
+    supplyIndex: 1
+  })
+})
+
+xit('dispatch should auto-supply coeffects', () => {
+  const { regEventFx, dispatch, regSupplier, getState, regAfter } = createStore()
+  regEventFx(evt.MESSAGE, { id }, ({ id }) => [
+    dbFx(R.assoc('id', id)),
+    dbFx(R.assoc('done', true))
+  ])
+  regSupplier('id', () => '88')
+  dispatch(evt.MESSAGE, 'hello')
+
+  expect(getState()).toEqual({
+    done: true,
+    id: '88'
+  })
+})
+
+xit('dispatch should auto-supply deep coeffects', () => {
+  const { regEventFx, regFx, regSupplier, regAfter, dispatch, subscribeToState } = createStore()
+  let stateCntr = 0
+  let state = null
+  let afterCntr = 0
+  subscribeToState(newState => {
+    stateCntr++
+    state = newState
+  })
+  regAfter((result) => {
+    console.log(result)
+  })
+  regFx('custom', () => {
+    afterCntr++
+  })
+  let idCounter = 10
+  regSupplier('id', () => idCounter++)
+  regEventFx(evt.MESSAGE, ({ id }), ({ id }, message) => [
+    dbFx(R.assoc('message', { id, message })),
+    dispatchFx(evt.SUBEVENT, message),
+    dbFx(R.assoc('done', true))
+  ])
+  regEventFx(evt.SUBEVENT, ({ id }), ({ id }, message) => [
+    ['custom', 'yep'],
+    dbFx(updateIn(['messages'], R.append(`sub ${message} ${id}`)))
+  ])
+  dispatch(evt.MESSAGE, 'hello')
+  expect(stateCntr).toEqual(1)
+  expect(afterCntr).toEqual(1)
+  expect(state).toEqual({
+    'done': true, 'message': {
+      id: 10,
+      message: 'hello'
+    }, 'messages': ['sub hello 11']
+  })
+})
--- src/createStore.js
+++ src/createStore.js
@@ -26,6 +26,7 @@ export const createStore = (initialState, ...middlewares) => {
     }
     return newStateOrReducer
   }
+
   const setState = (newStateOrReducer) => {
     appState = nextState(newStateOrReducer)
     stateListeners.forEach(fn => fn(appState))
@@ -60,77 +61,60 @@ export const createStore = (initialState, ...middlewares) => {
     fx[type] = fn
   }
 
-  let eventQueue = []
-
-  let dispatchScheduled = false
-  const scheduleDispatchProcessing = () => {
-    if (dispatchScheduled) return // no need, already scheduled
-    dispatchScheduled = true
-    setTimeout(processNextDispatch, 1)
-  }
-
   const notifyMiddlewares = (type, payload, effects,
     count) => initializedMiddlewares.forEach(m => m(type, payload, effects, count))
 
-  const processNextDispatch = () => {
-    dispatchScheduled = false
-    if (eventQueue.length === 0) return
-    const event = eventQueue.shift()
-    const type = event[0]
-    const args = event.slice(1)
-    // console.log('H:', type, args)
-    /* reframe only allows one handler I learned -- but I like extending event handlers elsewhere so multiple it is */
-    const eventHandlers = eventFx[type]
-    if (!eventHandlers) throw new Error(`No event fx handler for dispatched event "${type}". Try registering a handler using "regEventFx('${type}', ({ db }) => ({...some effects})"`)
-    let count = 0
-    eventHandlers.forEach(handler => {
-      const coeffects = { db: getState() }
-      // console.log(`event handler (${type}-${count})`, 'current db:', coeffects.db, 'args:', args)
-      const effects = handler(coeffects, ...event)
-      if (!effects) return
-      const effectsList = Array.isArray(effects) ? effects : Object.entries(effects)
-      /* Process effects */
-      effectsList.forEach(([key, value]) => {
-        const effect = fx[key]
-        // console.log(`  effect handler (${key}) for event (${type})`, value)
-        if (!effect) throw new Error(`No fx handler for effect "${key}". Try registering a handler using "regFx('${key}', ({ effect }) => ({...some side-effect})"`)
-        // NOTE: no need really to handle result of effect for now -
-        // we're not doing anything with promises for instance
-        effect(value)
+  let dispatchDepth = 0
+  const processDispatch = (event) => {
+    dispatchDepth = dispatchDepth + 1
+    try {
+      const type = event[0]
+      const args = event.slice(1)
+      // console.log('H:', type, args)
+      /* reframe only allows one handler I learned -- but I like extending event handlers elsewhere so multiple it is */
+      const eventHandlers = eventFx[type]
+      if (!eventHandlers) throw new Error(`No event fx handler for dispatched event "${type}". Try registering a handler using "regEventFx('${type}', ({ db }) => ({...some effects})"`)
+      let count = 0
+      eventHandlers.forEach(handler => {
+        const coeffects = { db: getState() }
+        // console.log(`event handler (${type}-${count})`, 'current db:', coeffects.db, 'args:', args)
+        const effects = handler(coeffects, ...args)
+        if (!effects) return
+        const effectsList = Array.isArray(effects) ? effects : Object.entries(effects)
+        /* Process effects */
+        effectsList.forEach(([key, value]) => {
+          const effect = fx[key]
+          // console.log(`  effect handler (${key}) for event (${type})`, value)
+          if (!effect) throw new Error(`No fx handler for effect "${key}". Try registering a handler using "regFx('${key}', ({ effect }) => ({...some side-effect})"`)
+          // NOTE: no need really to handle result of effect for now -
+          // we're not doing anything with promises for instance
+          effect(value)
+        })
+        // only notify if at root
+        if (dispatchDepth === 1) {
+          notifyMiddlewares(type, args, effects, count)
+          count++
+        }
       })
-      notifyMiddlewares(type, args, effects, count)
-      count++
-    })
-    if (eventQueue.length > 0) {
-      scheduleDispatchProcessing()
+    } finally {
+      dispatchDepth = dispatchDepth - 1
     }
   }
 
-  /* All dispatches are drained async */
+  /**
+   * synchronous dispatch
+   * @param event
+   */
   const dispatch = (...event) => {
-    if (event[0] instanceof Array) {
-      eventQueue.push(event[0])
-    } else {
-      eventQueue.push(event)
-    }
-    if (!eventQueue[eventQueue.length - 1][0]) throw new Error('Dispatch requires a valid event key')
-    processNextDispatch()
-  }
-
-  const dispatchAsync = (...event) => {
-    if (event[0] instanceof Array) {
-      eventQueue.push(event[0])
-    } else {
-      eventQueue.push(event)
-    }
-    if (!eventQueue[eventQueue.length - 1][0]) throw new Error('Dispatch requires a valid event key')
-    scheduleDispatchProcessing()
+    const finalEvent = Array.isArray(event[0]) ? event[0] : event
+    if (!finalEvent[0]) throw new Error('Dispatch requires a valid event key')
+    processDispatch(finalEvent)
   }
 
   regFx('db', setState)
 
   /* dispatch fx should happen next tick */
-  regFx('dispatch', dispatchAsync)
+  regFx('dispatch', dispatch)
 
   return {
     dispatch,
