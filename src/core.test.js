8f6f73c1e9bb74fbeff193790b2f174baae9465f
Alex Dixon <alex@conarrative.com>
2019-09-05 17:08:58 -0700

    First pass preFx

--- src/core.test.js
+++ src/core.test.js
@@ -1,5 +1,6 @@
 import * as R from 'ramda'
 import { createStore } from './createStore'
+import { derive } from './util'
 
 const evt = {
   MESSAGE: 'message',
@@ -167,5 +168,110 @@ describe('creation with specified env', () => {
 
     expect(env.eventFx['my-evt'].length).toEqual(2)
     expect(env.eventFx['my-evt'][1].name).toEqual('dynRegister')
+  })
+})
+describe('preFx', () => {
+  it('should work!', () => {
+    const userCredentials = { id: 123, username: 'mullet-man' }
+
+    const ls = () => {
+      let data = { user: JSON.stringify(userCredentials) }
+      return {
+        getItem: (k) => data[k],
+        setItem: (k, v) => data[k] = v,
+        removeItem: (k) => delete data[k]
+      }
+    }
+
+    const localStorage = ls()
+
+    const { env, regPreFx,  regEventFx, regFx, dispatch } =
+      createStore({ state: { db: {}, localStorage } } )
+
+    const deserializeLSUser = jest.fn(userStr => {
+      if (!userStr)  return null
+      const user = JSON.parse(userStr)
+      return updateIn(['username'], x => x.toUpperCase(), user)
+    })
+
+    // can we have derivations off of data that are memoized?
+    // const deserializeLocalStorageUser = R.memoizeWith(R.identity, deserializeLSUser)
+
+    // instead of something like this, wa can read directly from the reactive/stateful/ref thing
+    // and use a derive pattern for the value that was read
+    // const getLocalStorageUser = ls => {
+    //   const userStr = ls.getItem('user')
+    //   if (userStr) {
+    //     return deserializeLocalStorageUser(userStr)
+    //   }
+    // }
+
+    const lsKey = k => ls => ls.getItem(k)
+    const getUserStr = lsKey('user')
+    const getLocalStorageUser = derive([getUserStr], deserializeLSUser)
+
+    const getUser = R.prop('user')
+
+    const logOut = ({ state }) => {
+      if (getUser(state.db) && !getLocalStorageUser(state.localStorage)) {
+        throw new Error(
+          'localStorage and db state are out of sync, which may be intentional. '
+          + 'User may have been logged out by another tab, but is logged in here until next page refresh.'
+        )
+      }
+      state.localStorage.removeItem('user')
+      // setState(R.dissoc('user'))
+    }
+
+    // affects local storage
+    // could affect other things
+    const logIn = ({ state }, user) => {
+      state.localStorage.setItem('user', JSON.stringify(user))
+      // setState(R.assoc('user', user))
+    }
+
+    let redirects = 0
+    const redirect = () => {
+      redirects += 1
+    }
+    regFx('redirect', redirect)
+
+    regPreFx('user', ({ state: { db, localStorage } }) => {
+
+      return getLocalStorageUser(localStorage)
+
+      // const user = getUser(db)
+      // if (user) return user
+      // const lsUser = getLocalStorageUser(localStorage)
+      // if (lsUser) {
+      //   // writing side effects seems confusing / unexpected here
+      //   // we just want to supply a value based on a state that exists
+      //   //
+      //   // setState(R.assoc('user', lsUser))
+      //   return lsUser
+      // }
+    })
+
+    regEventFx('route/protected-page', ({ user }) => {
+      if (!user) return [['redirect', ['/']]]
+    })
+
+    dispatch('route/protected-page')
+    expect(redirects).toEqual(0)
+
+    // simulate other tab/process mutating the shared storage / logging out
+    localStorage.removeItem('user')
+    dispatch('route/protected-page')
+    getLocalStorageUser(env.state.localStorage)
+    getLocalStorageUser(env.state.localStorage)
+    expect(deserializeLSUser).toHaveBeenCalledTimes(1)
+
+    expect(()=>logOut(env)).not.toThrow()
+
+    logIn(env,R.assoc('username','fred',userCredentials))
+
+    // derive needs called with a new reference
+    expect(getLocalStorageUser(Object.assign({},env.state.localStorage)).username).toEqual('FRED')
+    expect(deserializeLSUser).toHaveBeenCalledTimes(2)
   })
 })
--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,6 @@
 // import hoistStatics from 'hoist-non-react-statics'
-import {mergeDeepRight} from 'ramda'
+// import mergeDeepRight from 'ramda/es/mergeDeepRight'
+import { mergeDeepRight } from 'ramda'
 
 export const regFxRaw = (env, type, fn) => {
   env.fx[type] = fn
@@ -8,8 +9,34 @@ export const regFxRaw = (env, type, fn) => {
 export const regEventFxRaw = (env, type, fn) => {
   env.eventFx[type] = [...env.eventFx[type] || [], fn]
 }
-export const regProviderRaw = (env, key, fn) => {
-  env.valueProviders[key] = fn
+
+export const regPreFxRaw = (env, type, fn) => {
+  env.preFx[type] = fn
+}
+
+const processEventEffects = (env, event) => {
+  const type = event[0]
+  const args = event.slice(1)
+  /* reframe only allows one handler I learned -- but I like extending event handlers elsewhere so multiple it is */
+  const eventHandlers = env.eventFx[type]
+  if (!eventHandlers) throw new Error(`No event fx handler for dispatched event "${type}". Try registering a handler using "regEventFx('${type}', ({ db }) => ({...some effects})"`)
+  let count = 0
+
+  eventHandlers.forEach(handler => {
+    const preFx = Object.entries(env.preFx).reduce((a, [k, f]) => {
+      a[k] = f(env)
+      return a
+    }, { eventName: type })
+
+    const effects = handler({ ...env.state, ...preFx }, ...args)
+
+    if (!effects) return
+    const effectsList = Array.isArray(effects) ? effects : Object.entries(effects)
+    processEffects(env, effectsList)
+
+    env.eventListeners.forEach(f => f(type, payload, effects, count))
+    count++
+  })
 }
 
 const checkType = (op, type) => {
@@ -17,10 +44,19 @@ const checkType = (op, type) => {
   if (type.length === 0) throw new Error(`${op} fx key cannot be a zero-length string`)
 }
 
+export const processEffects = (env, effects) => {
+  effects.forEach(([key, value]) => {
+    const effect = env.fx[key]
+    if (!effect) throw new Error(`No fx handler for effect "${key}". Try registering a handler using "regFx('${key}', ({ effect }) => ({...some side-effect})"`)
+    // NOTE: no need really to handle result of effect for now -
+    effect(env, value)
+  })
+}
+
 export const identityEnv = () => ({
   state: { db: {}, dispatch: { depth: 0 } },
-  valueProviders: {
-    db: ({ db }) => db
+  preFx: {
+    user: ({ localStorage }) => localStorage?localStorage.get('user'):'user'
   },
   fx: {
     db: (env, newStateOrReducer) => {
@@ -29,22 +65,44 @@ export const identityEnv = () => ({
       } else {
         env.state.db = newStateOrReducer(env.state.db)
         if (typeof env.state.db === 'function') {
-          throw new Error('db fxReg request was a reducer function that returned a function. ' + 'If you are using ramda, you probably didn\'t finish currying all the args')
+          throw new Error('db fxReg request was a reducer function that returned a function. '
+                          + 'If you are using ramda, you probably didn\'t finish currying all the args')
         }
+      }
+    },
+    dispatch: (env, event) => {
+      const finalEvent = Array.isArray(event[0]) ? event[0] : event
+      if (!finalEvent[0]) throw new Error('Dispatch requires a valid event key')
+
+      let startState
+      if (env.state.dispatch.depth === 0) {
+        startState = env.state.db
+      }
+
+      try {
+        env.state.dispatch.depth += 1
+        processEventEffects(env, finalEvent)
+      } finally {
+        // always make sure it decrements back even if fx throws error
+        env.state.dispatch.depth -= 1
+      }
+
+      if (env.state.dispatch.depth === 0 && startState !== env.state.db) {
+        env.dbListeners.forEach(fn => fn(env.state.db))
       }
     }
   },
   events: [],
   eventFx: {},
   dbListeners: [],
-  eventListeners: []
+  eventListeners: [],
 })
 
 export const createStore = (args = identityEnv()) => {
   const env = mergeDeepRight(identityEnv(), args)
 
-  /* Hacked a bit to support redux devtools -- the only middleware we care about right now */
-  let initializedEventListeners = env.eventListeners.map(m => m({}, {
+  /* Hacked a bit to support redux devtools  */
+  env.eventListeners.forEach(m => m({}, {
     setState(state) {
       env.fx.db(env, state)
     },
@@ -56,71 +114,10 @@ export const createStore = (args = identityEnv()) => {
     }
   }, {}))
 
-  const notifyEventListeners = (type, payload, effects,
-    count) => initializedEventListeners.forEach(m => m(type, payload, effects, count))
-
-  /**
-   * synchronous dispatch
-   * @param event
-   */
-  const processEventEffects = (env, event) => {
-    const type = event[0]
-    const args = event.slice(1)
-    // console.log('H:', type, args)
-    /* reframe only allows one handler I learned -- but I like extending event handlers elsewhere so multiple it is */
-    const eventHandlers = env.eventFx[type]
-    if (!eventHandlers) throw new Error(`No event fx handler for dispatched event "${type}". Try registering a handler using "regEventFx('${type}', ({ db }) => ({...some effects})"`)
-    let count = 0
-    eventHandlers.forEach(handler => {
-      const coeffects = Object.entries(env.valueProviders).reduce((a, [k, f]) => {
-        a[k] = f(env.state)
-        return a
-      }, { eventName: type })
-      const effects = handler(coeffects, ...args)
-      if (!effects) return
-      const effectsList = Array.isArray(effects) ? effects : Object.entries(effects)
-      /* Process effects */
-      effectsList.forEach(([key, value]) => {
-        const effect = env.fx[key]
-        // console.log(`  effect handler (${key}) for event (${type})`, value)
-        if (!effect) throw new Error(`No fx handler for effect "${key}". Try registering a handler using "regFx('${key}', ({ effect }) => ({...some side-effect})"`)
-        // NOTE: no need really to handle result of effect for now -
-        effect(env, value)
-      })
-
-      notifyEventListeners(type, args, effects, count)
-      count++
-    })
-  }
-
-  /* dispatch fxReg should happen next tick */
-  const dispatch = (env, event) => {
-    const finalEvent = Array.isArray(event[0]) ? event[0] : event
-    if (!finalEvent[0]) throw new Error('Dispatch requires a valid event key')
-
-    let startState
-    if (env.state.dispatch.depth === 0) {
-      startState = env.state.db
-    }
-
-    try {
-      env.state.dispatch.depth += 1
-      processEventEffects(env, finalEvent)
-    } finally {
-      // always make sure it decrements back even if fx throws error
-      env.state.dispatch.depth -= 1
-    }
-
-    if (env.state.dispatch.depth === 0 && startState !== env.state.db) {
-      env.dbListeners.forEach(fn => fn(env.state.db))
-    }
-  }
-
-  regFxRaw(env, 'dispatch', dispatch)
 
   return {
     env,
-    dispatch: (...event) => { dispatch(env, event) },
+    dispatch: (...event) => { env.fx.dispatch(env, event) },
     getState: () => env.state.db,
     setState: (newStateOrReducer) => env.fx.db(env, newStateOrReducer), // for when you want to bypass the eventing
     regEventFx: (type, fn) => {
@@ -128,11 +125,12 @@ export const createStore = (args = identityEnv()) => {
       env.eventFx[type] = [...env.eventFx[type] || [], fn]
     },
     regFx: (type, fn) => {
-      checkType('regEventFx', type)
+      checkType('regFx', type)
       env.fx[type] = fn
     },
-    regProvider: (key, fn) => {
-      env.valueProviders[key] = fn
+    regPreFx: (type, fn) => {
+      checkType('regPreFx', type)
+      env.preFx[type] = fn
     },
     subscribeToState: fn => env.dbListeners.push(fn)
   }
