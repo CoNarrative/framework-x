4b0f1159f79df91f311543a8c029021060e2896b
Austin Lee <heero916@hotmail.com>
2019-10-01 10:57:09 -0700

    update footer links

--- site/src/components/Footer.js
+++ site/src/components/Footer.js
@@ -28,38 +28,63 @@ const Header = styled.div({
   marginBottom: 16,
 })
 
-const Navlink = ({children, to}) => (
-  <Link to={to} css={{
-    fontSize: '1rem',
-    fontWeight: 600,
-    textDecoration: 'none',
-    color: theme.black,
-    marginLeft: 12,
-    marginBottom: 12,
-    letterSpacing: '-0.014rem',
-  }}>{children}</Link>
+const Navlink = ({children, to, href}) => (
+  <React.Fragment>
+    {href ? <a href={href} css={{
+        fontSize: '1rem',
+        fontWeight: 600,
+        textDecoration: 'none',
+        color: theme.black,
+        marginLeft: 12,
+        marginBottom: 12,
+        letterSpacing: '-0.014rem',
+      }}>{children}</a> :
+      <Link to={to} css={{
+        fontSize: '1rem',
+        fontWeight: 600,
+        textDecoration: 'none',
+        color: theme.black,
+        marginLeft: 12,
+        marginBottom: 12,
+        letterSpacing: '-0.014rem',
+      }}>{children}</Link>}
+  </React.Fragment>
 )
 
 export const Footer = ({floating}) => (
-  <Banner rootCss={{ borderTop: '1px solid ' + theme.black, position: floating ? 'absolute' : 'relative', left: 0, right: 0, [whenTablet]: {paddingBottom: 0}}} wrapCss={{[whenTablet]: {flexDirection: 'column-reverse', alignItems: 'center'},[whenMobile]: {alignItems: 'flex-start', paddingLeft: 40, paddingRight: 24}, }}>
-    <div css={{ [whenTablet]: {marginTop: 48, alignSelf: 'flex-start', position: 'absolute', left: 32, bottom: 32 }}}>
-      <div css={{fontFamily: '', color: theme.darkGrey, fontSize: '1rem', marginBottom: 8,}}>Built with...</div>
+  <Banner rootCss={{
+    borderTop: '1px solid ' + theme.black,
+    position: floating ? 'absolute' : 'relative',
+    left: 0,
+    right: 0,
+    [whenTablet]: {paddingBottom: 0}
+  }} wrapCss={{
+    [whenTablet]: {flexDirection: 'column-reverse', alignItems: 'center'},
+    [whenMobile]: {alignItems: 'flex-start', paddingLeft: 40, paddingRight: 24},
+  }}>
+    <div css={{[whenTablet]: {marginTop: 48, alignSelf: 'flex-start', position: 'absolute', left: 32, bottom: 32}}}>
+      {/*<div css={{fontFamily: '', color: theme.darkGrey, fontSize: '1rem', marginBottom: 8,}}>Built with...</div>*/}
       <div css={{fontFamily: 'Basier Square Mono', fontSize: '1rem'}}>@2019 CoNarrative Inc.</div>
     </div>
-    <div css={{display: 'flex', [whenTablet]: {paddingBottom: 144,}, [whenMobile]:{flexDirection: 'column',}}}>
-      <div css={{display: 'flex', flexDirection: 'column', marginRight: 120, '& > :last-child': {marginBottom: 0}, [whenMobile]: {marginRight: 0, marginBottom: 80}}}>
+    <div css={{display: 'flex', [whenTablet]: {paddingBottom: 144,}, [whenMobile]: {flexDirection: 'column',}}}>
+      <div css={{
+        display: 'flex',
+        flexDirection: 'column',
+        marginRight: 120,
+        '& > :last-child': {marginBottom: 0},
+        [whenMobile]: {marginRight: 0, marginBottom: 80}
+      }}>
         <Header>Resources</Header>
-        <Navlink>Github</Navlink>
-        <Navlink>Documentation</Navlink>
-        <Navlink>Examples</Navlink>
-        <Navlink>Community</Navlink>
+        <Navlink href={'https://github.com/CoNarrative/framework-x/'}>Github</Navlink>
+        <Navlink to={'/api'}>Documentation</Navlink>
+        <Navlink to={'/learn'}>Learn</Navlink>
+        {/*<Navlink to={'/examples'}>Examples</Navlink>*/}
       </div>
       <div css={{display: 'flex', flexDirection: 'column', '& > :last-child': {marginBottom: 0}}}>
         <Header>Connect with us!</Header>
-        <Navlink>Twitter</Navlink>
-        <Navlink>reddit</Navlink>
-        <Navlink>Github issues</Navlink>
-        <Navlink>E-mail</Navlink>
+        <Navlink href={'https://twitter.com/framework_x'}>Twitter</Navlink>
+        <Navlink href={'https://www.reddit.com/r/framework_x/'}>reddit</Navlink>
+        <Navlink href={'https://github.com/CoNarrative/framework-x/issues'}>Github issues</Navlink>
       </div>
       <div></div>
     </div>
