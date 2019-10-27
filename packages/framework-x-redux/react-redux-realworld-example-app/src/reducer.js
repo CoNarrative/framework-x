37a22213d7af7a3db0c9dff7707c3d7b5a22fbea
Alex Dixon <alex@conarrative.com>
2019-10-27 08:04:09 -0700

    [fx-redux] idnentity editor reducer not necessary anymore

--- packages/framework-x-redux/react-redux-realworld-example-app/src/reducer.js
+++ packages/framework-x-redux/react-redux-realworld-example-app/src/reducer.js
@@ -3,7 +3,7 @@ import articleList from './reducers/articleList';
 import auth from './reducers/auth';
 import { combineReducers } from 'redux';
 import common from './reducers/common';
-import editor from './reducers/editor';
+// import editor from './reducers/editor';
 import home from './reducers/home';
 import profile from './reducers/profile';
 import settings from './reducers/settings';
@@ -14,7 +14,6 @@ export default combineReducers({
   articleList,
   auth,
   common,
-  editor:(state={})=>state,
   home,
   profile,
   settings,
