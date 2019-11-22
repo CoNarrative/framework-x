1698a6ecb9008fb7f94bc8a4fc4ce2a26d935861
Alex Dixon <alex@conarrative.com>
2019-11-22 07:13:48 -0800

    Add sample anim sections

--- site/src/templates/index.js
+++ site/src/templates/index.js
@@ -1,30 +1,45 @@
 import React from 'react'
 import MulletManFluid from '../components/image'
-import {DimensionalBox} from '../components/DimensionalBox'
-import {Highlight} from '../components/TextBackgroundHighlight'
+import { DimensionalBox } from '../components/DimensionalBox'
+import { Highlight } from '../components/TextBackgroundHighlight'
 import CircleBackdrop from '../images/circle-backdrop.svg'
 import HelpDesk from '../images/help-desk.svg'
-import {Input, MultilineInput} from "../components/Input";
-import {Button, Button3D} from "../components/Button";
+import { Input, MultilineInput } from '../components/Input'
+import { Button, Button3D } from '../components/Button'
 import Enter from '../assets/icons/enter.svg'
-import {Link} from 'gatsby'
+import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import {Footer} from "../components/Footer";
-import * as axios from "axios";
-import {whenMobile, whenTablet} from "../theme";
+import { Footer } from '../components/Footer'
+import * as axios from 'axios'
+import { whenMobile, whenTablet } from '../theme'
 import MulletMan from '../assets/icons/mascot.svg'
 import * as theme from '../theme'
-import {Shapes} from "../images/Shapes";
+import { Shapes } from '../images/Shapes'
 
 
 const MulletManMain = () =>
-  <div css={{width: '60%', marginRight: '2.4rem', [whenMobile]: {alignSelf: 'center', marginTop: 40, width: '30%', marginBottom: 40, marginRight: 0,}}}>
-    <img src={MulletMan}/>
+  <div css={{
+    width: '60%',
+    marginRight: '2.4rem',
+    [whenMobile]: {
+      alignSelf: 'center',
+      marginTop: 40,
+      width: '30%',
+      marginBottom: 40,
+      marginRight: 0,
+    }
+  }}>
+    <img src={MulletMan} />
   </div>
 
-export const Banner = ({children, rootCss, wrapCss}) => (
-  <div css={{display: 'flex', justifyContent: 'center', paddingTop: 64, paddingBottom: 80, ...rootCss,}}>
+export const Banner = ({ children, rootCss, wrapCss }) => (
+  <div css={{
+    display: 'flex',
+    justifyContent: 'center',
+    paddingTop: 64,
+    paddingBottom: 80, ...rootCss,
+  }}>
     <div css={{
       width: '100vw',
       maxWidth: 1440,
@@ -42,14 +57,34 @@ export const Banner = ({children, rootCss, wrapCss}) => (
 )
 
 const Features = () => (
-  <Banner rootCss={{backgroundColor: theme.black, color: 'white'}} wrapCss={{
-    [whenTablet]: {flexDirection: 'column',},
-    [whenMobile]: {paddingLeft: 24, paddingRight: 24,}
+  <Banner rootCss={{ backgroundColor: theme.black, color: 'white' }} wrapCss={{
+    [whenTablet]: { flexDirection: 'column', },
+    [whenMobile]: { paddingLeft: 24, paddingRight: 24, }
   }}>
-    <Shapes/>
-    <div css={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start', flexShrink: 1, maxWidth: 540, [whenTablet]: {marginTop: 40,}}}>
-      <h1 css={{marginTop: 0, fontSize: '3rem', marginBottom: '1.2rem', display: 'flex'}}>Key features</h1>
-      <ol css={{fontFamily: 'Basier Square Mono', fontSize: '1.04rem', margin: 0, marginBottom: 20, lineHeight: '1.6rem', color: '#CBD2E4', '& > li': {marginBottom: 12,}}}>
+    <Shapes />
+    <div css={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+      flexShrink: 1,
+      maxWidth: 540,
+      [whenTablet]: { marginTop: 40, }
+    }}>
+      <h1 css={{
+        marginTop: 0,
+        fontSize: '3rem',
+        marginBottom: '1.2rem',
+        display: 'flex'
+      }}>Key features</h1>
+      <ol css={{
+        fontFamily: 'Basier Square Mono',
+        fontSize: '1.04rem',
+        margin: 0,
+        marginBottom: 20,
+        lineHeight: '1.6rem',
+        color: '#CBD2E4',
+        '& > li': { marginBottom: 12, }
+      }}>
         <li>Less code</li>
         <li>Spreadsheet programming</li>
         <li>Single source of truth</li>
@@ -61,15 +96,35 @@ const Features = () => (
     </div>
   </Banner>
 )
+const Additive = () => (
+  <Banner rootCss={{ backgroundColor: '#FFFFFF', color: theme.black }} wrapCss={{
+    [whenTablet]: { flexDirection: 'column', },
+    [whenMobile]: { paddingLeft: 24, paddingRight: 24, }
+  }}>
+    <div>text or img</div>
+    <div>text or img</div>
+  </Banner>
+)
 
 const ContactUs = () => (
-  <Banner rootCss={{backgroundColor: '#E4E6EB'}} wrapCss={{
-    [whenTablet]: {flexDirection: 'column-reverse',},
-    [whenMobile]: {paddingLeft: 24, paddingRight: 24,}
+  <Banner rootCss={{ backgroundColor: '#E4E6EB' }} wrapCss={{
+    [whenTablet]: { flexDirection: 'column-reverse', },
+    [whenMobile]: { paddingLeft: 24, paddingRight: 24, }
   }}>
-    <div css={{display: 'flex', flexDirection: 'column', flexShrink: 1, maxWidth: 540,}}>
-      <h1 css={{marginTop: 0, fontSize: '3rem', marginBottom: '0.8rem', display: 'flex'}}>Need a helping hand?</h1>
-      <span css={{fontFamily: 'Basier Square Mono', fontSize: '1.04rem', marginBottom: 20, lineHeight: '1.6rem',}}>Please feel free to reach out to us at anytime to discuss our consulting services.</span>
+    <div
+      css={{ display: 'flex', flexDirection: 'column', flexShrink: 1, maxWidth: 540, }}>
+      <h1 css={{
+        marginTop: 0,
+        fontSize: '3rem',
+        marginBottom: '0.8rem',
+        display: 'flex'
+      }}>Need a helping hand?</h1>
+      <span css={{
+        fontFamily: 'Basier Square Mono',
+        fontSize: '1.04rem',
+        marginBottom: 20,
+        lineHeight: '1.6rem',
+      }}>Please feel free to reach out to us at anytime to discuss our consulting services.</span>
       <div>
         {/*<DimensionalBox handleHeight={true} rootCss={{*/}
         {/*  padding: '16px 24px',*/}
@@ -104,66 +159,77 @@ const ContactUs = () => (
       width: '40%',
       maxHeight: 360,
       marginLeft: 80,
-      [whenTablet]: {marginLeft: 0, marginBottom: 64, width: '70%', minWidth: 280}
-    }} src={HelpDesk}/>
+      [whenTablet]: { marginLeft: 0, marginBottom: 64, width: '70%', minWidth: 280 }
+    }} src={HelpDesk} />
   </Banner>
 
 )
 
-const MainContent = ({starCount, downloadCount}) =>
+const MainContent = ({ starCount, downloadCount }) =>
   <div css={{
     display: 'flex',
     maxWidth: 960,
     alignSelf: 'center',
     paddingBottom: '4rem',
-    [whenMobile]: {flexDirection: 'column', padding: '0 24px', paddingBottom: '3rem'}
+    [whenMobile]: { flexDirection: 'column', padding: '0 24px', paddingBottom: '3rem' }
   }}>
-    <MulletManMain/>
-    <div css={{display: 'flex', flexDirection: 'column'}}>
-      <h1 css={{fontSize: '3rem', marginBottom: '0.8rem', display: 'flex', flexWrap: 'wrap', marginTop: 0}}>
-        Reasonable <Highlight rootCss={{marginLeft: 10}}>global state.</Highlight>
+    <MulletManMain />
+    <div css={{ display: 'flex', flexDirection: 'column' }}>
+      <h1 css={{
+        fontSize: '3rem',
+        marginBottom: '0.8rem',
+        display: 'flex',
+        flexWrap: 'wrap',
+        marginTop: 0
+      }}>
+        Reasonable <Highlight rootCss={{ marginLeft: 10 }}>global state.</Highlight>
       </h1>
-      <div css={{marginBottom: '1.45rem', fontFamily: 'Basier Square Mono', lineHeight: '1.7rem'}}>
+      <div css={{
+        marginBottom: '1.45rem',
+        fontFamily: 'Basier Square Mono',
+        lineHeight: '1.7rem'
+      }}>
         Framework-X is a Javascript framework that processes definitions of cause and
         effect.
         Applications define what their events mean by mapping them to descriptions of the
         effects they entail -- a state change, an
         asynchronous API call, the invocation of a custom function, or another event.
       </div>
-      <div css={{ display: 'flex', [whenMobile]: {flexDirection: 'column'}}}>
-        <div css={{alignSelf: 'flex-start', [whenMobile]: {width: '100%'}}}>
-          <DimensionalBox handleHeight={true} rootCss={{alignItems: 'center'}}>
+      <div css={{ display: 'flex', [whenMobile]: { flexDirection: 'column' } }}>
+        <div css={{ alignSelf: 'flex-start', [whenMobile]: { width: '100%' } }}>
+          <DimensionalBox handleHeight={true} rootCss={{ alignItems: 'center' }}>
             <div css={{
               display: 'flex',
               justifyContent: 'center',
               width: 520,
               paddingTop: 9,
               paddingBottom: 9,
-              '& > :not(:last-child)': {marginRight: 64},
+              '& > :not(:last-child)': { marginRight: 64 },
               [whenMobile]: {
                 flexDirection: 'column',
                 width: '100%',
                 paddingLeft: 24,
                 paddingTop: 16,
                 paddingBottom: 16,
-                '& > :not(:last-child)': {marginRight: 0, marginBottom: 12},
+                '& > :not(:last-child)': { marginRight: 0, marginBottom: 12 },
               },
             }}>
               <span>License: MIT</span>
               <a href={'https://github.com/CoNarrative/framework-x'}
                  target={'_blank'}
-                 style={{color: 'inherit', textDecoration: 'inherit'}}>
+                 style={{ color: 'inherit', textDecoration: 'inherit' }}>
                 +{starCount} stars
               </a>
               <a href={'https://www.npmjs.com/package/framework-x'}
                  target={'_blank'}
-                 style={{color: 'inherit', textDecoration: 'inherit'}}>
+                 style={{ color: 'inherit', textDecoration: 'inherit' }}>
                 +{downloadCount} downloads
               </a>
             </div>
           </DimensionalBox>
         </div>
-        <Link to={'/api'} css={{marginLeft: 32, [whenMobile]:{marginLeft: 0, marginTop: 24}}}>
+        <Link to={'/api'}
+              css={{ marginLeft: 32, [whenMobile]: { marginLeft: 0, marginTop: 24 } }}>
           <Button3D>Go to documentation</Button3D>
         </Link>
       </div>
@@ -182,21 +248,21 @@ const getGithubStarCount = async () => {
   const headers = {
     'Authorization': `token ${GITHUB_TOKEN}`,
   }
-  const res = await axios({url: ghRepoUrl, headers})
+  const res = await axios({ url: ghRepoUrl, headers })
 
-  const {stargazers_count} = res.data
+  const { stargazers_count } = res.data
   return stargazers_count
 }
 
 class MainContentAsyncStuff extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {starCount: '--', downloadCount: '--'}
+    this.state = { starCount: '--', downloadCount: '--' }
   }
 
   UNSAFE_componentWillMount() {
     Promise.all([getNPMDownloadCount(), getGithubStarCount()]).then(([a, b]) => {
-      this.setState({starCount: b, downloadCount: a})
+      this.setState({ starCount: b, downloadCount: a })
     })
   }
 
@@ -214,7 +280,7 @@ const IndexPage = () => {
         height: 'calc(100vh - 64px)',
       },
     }}>
-      <SEO title="framework-x: Reasonable global state"/>
+      <SEO title="framework-x: Reasonable global state" />
 
       <div css={{
         display: 'flex',
@@ -230,11 +296,41 @@ const IndexPage = () => {
           height: 'unset',
         }
       }}>
-        <MainContentAsyncStuff/>
+        <MainContentAsyncStuff />
       </div>
-      <Features/>
-      <ContactUs/>
-      <Footer/>
+      {[{
+        title: 'Add without breaking',
+        blurb:'',
+      },
+        { title: '40% less code',
+          blurb:'XXXXXXX XXXXXXXXXXX XXXXXXXXX'
+        },
+        { title: 'temp',
+          blurb:''
+        }
+      ].map(({ title,blurb }, i) =>
+        <Banner
+          rootCss={i % 2 !== 0 ?
+            {
+              backgroundColor: '#FFFFFF', color: theme.black,
+              flexDirection: 'row-reverse'
+            } :
+            { color: '#FFFFFF', backgroundColor: theme.black }
+          }
+          wrapCss={Object.assign({}, i % 2 !== 0 ? { flexDirection: 'row-reverse' } : {},
+            {
+              [whenTablet]: { flexDirection: 'column', },
+              [whenMobile]: { paddingLeft: 24, paddingRight: 24, }
+            })}>
+          <div>
+          <h1>{title}</h1>
+            {blurb}
+          </div>
+          <div>text or img</div>
+        </Banner>)}
+      <Features />
+      <ContactUs />
+      <Footer />
 
     </Layout>
   )
